ce589b974e35803e6322a77d6c9aadbca65bd043
Peter Pham <phamhere@gmail.com>
2019-02-24 13:40:07 -0700

    truncated summary and pub_date, added blogdetail page

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    # name displayed in admin page
+    def __str__(self):
+        return self.title
+
+    # functions to get better properties of class when called in templates
+    def summary(self):
+        return self.body[:150]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs, name='allblogs.html')
+    path('', views.allblogs, name='allblogs.html'),
+    path('<int:blog_id>/', views.detail, name='detail')
 ]
